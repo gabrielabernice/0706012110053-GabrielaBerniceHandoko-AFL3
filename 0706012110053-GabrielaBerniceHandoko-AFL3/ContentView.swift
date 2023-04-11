49db63c48f54b89014638e5f18d0e3f578cfdbf8
gabrielabernice <gbernice@student.ciputra.ac.id>
2023-04-11 09:18:40 +0700

    chapter 1 - section 6

--- 0706012110053-GabrielaBerniceHandoko-AFL3/ContentView.swift
+++ 0706012110053-GabrielaBerniceHandoko-AFL3/ContentView.swift
@@ -9,18 +9,27 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack(alignment: .leading) {
-            Text("Turle Rock")
-                .font(.title)
-            HStack {
-                Text("Joshua Tree National Park")
-                    .font(.subheadline)
-                Spacer()
-                Text("California")
-                    .font(.subheadline)
+        VStack {
+            MapView()
+                .frame(height: 300)
+            
+            CircleImage()
+                .offset(y: -130)
+                .padding(.bottom, -130)
+            
+            VStack(alignment: .leading) {
+                Text("Turle Rock")
+                    .font(.title)
+                HStack {
+                    Text("Joshua Tree National Park")
+                    Spacer()
+                    Text("California")
+                }
+                .font(.subheadline)
+                .foregroundColor(.secondary)
             }
+            .padding()
         }
-        .padding()
     }
 }
 
