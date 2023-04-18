78d28901ee755d31cc500e5f9991ad233450326f
gabrielabernice <gbernice@student.ciputra.ac.id>
2023-04-18 19:32:49 +0700

    chapter 4 - part 3 - section 1, 2, 3, 4

--- 0706012110053-GabrielaBerniceHandoko-AFL3/Views/Landmarks/LandmarkRow.swift
+++ 0706012110053-GabrielaBerniceHandoko-AFL3/Views/Landmarks/LandmarkRow.swift
@@ -9,32 +9,36 @@ import SwiftUI
 
 struct LandmarkRow: View {
     var landmark: Landmark
-
-    // to make show the landmark image, resize it and give the name
+    
     var body: some View {
         HStack {
             landmark.image
                 .resizable()
                 .frame(width: 50, height: 50)
                 .cornerRadius(5)
-            Text(landmark.name)
-
+            VStack(alignment: .leading) {
+                Text(landmark.name)
+                    .bold()
+                Text(landmark.park)
+                    .font(.caption)
+                    .foregroundColor(.secondary)
+            }
+            
             Spacer()
-
-            // it it is favorite, then the star will be filled with yellow color
+            
             if landmark.isFavorite {
                 Image(systemName: "star.fill")
                     .imageScale(.medium)
                     .foregroundColor(.yellow)
             }
         }
+        .padding(.vertical, 4)
     }
 }
 
-// to show the preview of landmark row
 struct LandmarkRow_Previews: PreviewProvider {
     static var landmarks = ModelData().landmarks
-
+    
     static var previews: some View {
         Group {
             LandmarkRow(landmark: landmarks[0])
